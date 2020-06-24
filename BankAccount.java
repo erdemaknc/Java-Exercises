d0b9e210a57a398b13ba6be24d5aec86dcdc2ab0
Erdem akinci <45974042+erdemaknc@users.noreply.github.com>
2020-06-25 00:19:21 +0300

    SimpleBankAccount

---
+++ BankAccount.java
@@ -0,0 +1,73 @@
+package erdem;
+
+public class BankAccount {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
+    public BankAccount(String number, double balance, String customerName, String email, String phoneNumber) {
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void deposit(double depositAmount) {
+        this.balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + "made.New balance is " + this.balance);
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        if (this.balance - withdrawalAmount < 0) {
+            System.out.println("Only" + this.balance + "available.Withdrawal not processed");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + "proceed.Remaining balance is " + this.balance);
+        }
+
+    }
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
